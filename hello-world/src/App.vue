3c5a8f9ec947676d731127ae3e16d1559447accc
Pepojmc <pepojmc@gmail.com>
2022-09-04 23:13:05 +0200

    Done

---
+++ hello-world/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <About class="aboutComponent"/>
+</template>
+
+<script>
+import About from './components/About.vue';
+export default {
+    components: { About },
+}
+
+
+</script>
+
+<style>
+
+</style>
---
+++ hello-world/src/components/About.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="textDiv1">
+    Esto es un texo de prueba que aparece procedente del componente de <strong>Vue.js</strong>: About.vue.
+  </div>
+
+  <span>Message: {{ aboutMe }}</span>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      aboutMe: "Lorem ipsum"
+    }
+  },
+
+}
+</script>
+
+<style scoped>
+  .textDiv1 {
+    background-color: #34495E;
+    color: #ffffff;
+    text-align: center;
+    padding: 2%;
+    font-size: 1.3em;
+  }
+  strong {
+    color: #41B883
+  }
+</style>
